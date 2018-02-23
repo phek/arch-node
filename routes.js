becbd376b517e2e2a8dafc5f5664378e20f0566b
Philip Ekblom <philip.ekblom@gmail.com>
2018-02-23 04:24:37 +0100

    Added error mock and run.bat

--- routes.js
+++ routes.js
@@ -4,10 +4,31 @@ router.get('/', function(req, res){
     res.status(200).json();
 });
 
-router.post('/authenticate', function(req, res){
+/* Mock Success */
+router.post('/authentication', function(req, res){
     res.status(200).json({
         token: req.body.username + '-' + req.body.password
     });
 });
+
+router.post('/registration', function(req, res){
+    res.status(200).json({
+        token: req.body
+    });
+});
+
+/* Mock Error
+router.post('/authentication', function(req, res){
+    res.status(200).json({
+        error: 'INVALID_USER'
+    });
+});
+
+router.post('/registration', function(req, res){
+    res.status(200).json({
+        error: 'USERNAME_UNAVAILABLE'
+    });
+});
+*/
 
 module.exports = router;
