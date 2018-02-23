b6b7022715a9e5148f565be9247730f6cc38a94d
Philip Ekblom <philip.ekblom@gmail.com>
2018-02-23 06:48:59 +0100

    Added error toggle

--- routes.js
+++ routes.js
@@ -1,36 +1,38 @@
 const router = require('express').Router();
+const ERROR = true;
 
 router.get('/', function(req, res){
     res.status(200).json();
 });
 
-/* Mock Success */
-router.post('/authentication', function(req, res){
-    res.status(200).json({
-		token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-        //token: req.body.username + '-' + req.body.password
-    });
-});
+if (!ERROR) {
+	/* Mock Success */
+	router.post('/authentication', function(req, res){
+		res.status(200).json({
+			//token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+			token: req.body.username + '-' + req.body.password
+		});
+	});
 
-router.post('/registration', function(req, res){
-    res.status(200).json({
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-		//token: req.body
-    });
-});
+	router.post('/registration', function(req, res){
+		res.status(200).json({
+			token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+			//token: req.body
+		});
+	});
+} else {
+	/* Mock Error */
+	router.post('/authentication', function(req, res){
+		res.status(200).json({
+			error: 'INVALID_USER'
+		});
+	});
 
-/* Mock Error
-router.post('/authentication', function(req, res){
-    res.status(200).json({
-        error: 'INVALID_USER'
-    });
-});
-
-router.post('/registration', function(req, res){
-    res.status(200).json({
-        error: 'USERNAME_UNAVAILABLE'
-    });
-});
-*/
+	router.post('/registration', function(req, res){
+		res.status(200).json({
+			error: 'USERNAME_UNAVAILABLE'
+		});
+	});
+}
 
 module.exports = router;
